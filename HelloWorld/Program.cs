10c4935ade051520463b356a7473ed1825b685a6
hbblo <hbblo@LAPTOP-UJKTBKSO>
2023-02-17 08:41:46 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string imie = "Henryk";
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Nazywam się Henryk Blok");
+
+
+        }
+
+    }
+}
+
